3531fea759cc7da81962fe68d7aa6d37917a76ea
Priyansu Kumar Nayak <priyansu18.8@gmail.com>
2022-12-28 15:14:51 +0530

    updated

---
+++ index.js
@@ -0,0 +1,68 @@
+var numOfDrumButtons = document.querySelectorAll(".drum").length;
+
+var buttonCollection = document.querySelectorAll(".drum");
+
+//detecting which button was pressed
+for (var i = 0; i < numOfDrumButtons; i++) {
+
+  buttonCollection[i].addEventListener("click", function() {
+
+      var buttonInnerHtml = this.innerHTML;
+      makeSound(buttonInnerHtml);
+      buttonAnimation(buttonInnerHtml);
+    }//anonymous function
+  );//eventListener
+  }//for loop
+
+//Detecting Keyboard Press
+  document.addEventListener("keydown",function(event){
+    console.log(event);
+    makeSound(event.key);
+    buttonAnimation(event.key);
+  }//anonymous
+);//eventListener
+
+function buttonAnimation(currentKey){
+  var activeButton=document.querySelector("."+currentKey);
+  activeButton.classList.add("pressed");
+  setTimeout(function()
+  {
+    activeButton.classList.remove("pressed");
+  },100);
+}
+
+  function makeSound(key){
+    switch (key) {
+      case "w":
+        var tom1 = new Audio('sounds/tom-1.mp3');
+        tom1.play();
+        break;
+      case "s":
+        var tom2 = new Audio('sounds/tom-2.mp3');
+        tom2.play();
+        break;
+      case "a":
+        var tom3 = new Audio('sounds/tom-3.mp3');
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio('sounds/tom-4.mp3');
+        tom4.play();
+        break;
+      case "j":
+        var snare = new Audio('sounds/snare.mp3');
+        snare.play();
+        break;
+      case "k":
+        var crash = new Audio('sounds/crash.mp3');
+        crash.play();
+        break;
+      case "l":
+        var kick = new Audio('sounds/kick-bass.mp3');
+        kick.play();
+        break;
+      default:
+        console.log("Invalid");
+    }
+  }
+       //switch
